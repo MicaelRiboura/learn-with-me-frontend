cc8452fb2cfd6a815866a14919967e9ec5fa4d2c
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-09-17 00:26:08 -0300

    feat(App): add base layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 
+import Header from './components/template/Header';
+import Banner from './components/template/Banner';
+import Content from './components/template/Content';
+
+
 function App() {
   return (
     <div className="App">
-      
+        <Header />
+        <Banner />
+        <Content />
     </div>
   );
 }
