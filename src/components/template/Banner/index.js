2bc1c1ffba2eb6a33ac7c8436b4e2de9b40ee61a
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-09-17 14:40:48 -0300

    fix(Banner): remove unused hooks and adjust className

--- src/components/template/Banner/index.js
+++ src/components/template/Banner/index.js
@@ -1,18 +1,9 @@
-import { useEffect, useState } from 'react';
 import './styles.css';
 
 import videoBg from '../../assets/bannerVideo.mp4';
 import BaseContainer from '../BaseContainer';
 
 export default function Banner() {
-
-    const [play, setPlay] = useState(false);
-
-    useEffect(() => {
-        setTimeout(() => {
-            setPlay(true);
-        }, 1000);
-    }, []);
 
     return (
         <div className="banner-container">
@@ -25,7 +16,7 @@ export default function Banner() {
                 muted={true}
             />
             <BaseContainer>
-            <div class="hero-area">
+            <div className="hero-area">
                 <h1 className="hero-title"><span>Compartilhe</span> seus estudos e <span>aprenda</span> com outras pessoas.</h1>
             </div>
             </BaseContainer>
