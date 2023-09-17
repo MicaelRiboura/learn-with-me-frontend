f2cd11c0f3ffabe358e7028e27b9a1bc0ec962c4
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-09-17 19:59:34 -0300

    feat(CreateUserForm): add submit and validation in form

--- src/components/CreateUserForm/index.js
+++ src/components/CreateUserForm/index.js
@@ -1,21 +1,73 @@
+import { toast } from "react-toastify";
+import { useValidationForms } from "../../hooks/useValidationForms";
+import { useLearnModal } from "../../contexts/ModalContextApiHook";
+
 export default function CreateUserForm() {
+    const { validateRequiredFields, validateEmailField } = useValidationForms();
+    const { hideModal } = useLearnModal();
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        console.log('data: ', e.target.elements);
+
+        const name = e.target.elements['name'].value;
+        const email = e.target.elements['email'].value;
+        const password = e.target.elements['password'].value;
+
+        let validationErrors = validateRequiredFields([
+            { input: name, msg: 'O campo nome é obrigatório' },
+            { input: email, msg: 'O campo e-mail é obrigatório' },
+            { input: password, msg: 'O campo senha é obrigatório' },
+        ]);
+
+        validationErrors += validateEmailField(email);
+
+        if (validationErrors === 0) {
+            console.log('sem erro');
+            const formData = new FormData();
+            formData.append('name', name);
+            formData.append('email', email);
+            formData.append('password', password);
+            
+            try {
+                const url = 'http://localhost:5000/users/create';
+                const responseJson = await fetch(url, {
+                    method: 'post',
+                    body: formData
+                })
+                    // .then((response) => response.json())
+                const response = await responseJson.json();
+                console.log('response: ', response);
+                if (!response.mesage) {
+                    toast.success('Cadastro realizado com sucesso!');
+                    hideModal('createUser');
+                } else {
+                    toast.error(response.mesage);
+                }
+            } catch(error) {
+                console.error('Error:', error);
+                toast.error('Erro no cadastro!');
+            }
+        }
+    }
+
     return (
-        <div>
+        <form method="post" onSubmit={onSubmit}>
             <div className="input-group">
                 <label htmlFor="">Nome</label>
-                <input type="text" className="form-input" />
+                <input name="name" type="text" className="form-input" />
             </div>
             <div className="input-group">
                 <label htmlFor="">E-mail</label>
-                <input type="text" className="form-input" />
+                <input name="email" type="text" className="form-input" />
             </div>
             <div className="input-group">
                 <label htmlFor="">Senha</label>
-                <input type="password" className="form-input" />
+                <input name="password" type="password" className="form-input" />
             </div>
             <div className="form-button-area">
-                <button className="learn-btn learn-btn-primary">Cadastre-se</button>
+                <button type="submit" className="learn-btn learn-btn-primary">Cadastre-se</button>
             </div>
-        </div>
+        </form>
     )
 }
