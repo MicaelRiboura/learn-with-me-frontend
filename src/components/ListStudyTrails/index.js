500c6ecf9e975a5aa50173b182b81dbd0d80571f
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-09-17 20:49:18 -0300

    feat: add hasAuthor param in ListStudyTrails and StudyTrailCard

--- src/components/ListStudyTrails/index.js
+++ src/components/ListStudyTrails/index.js
@@ -1,13 +1,13 @@
 import StudyTrailCard from '../StudyTrailCard';
 import './styles.css';
 
-export default function ListStudyTrails({ title = 'Trilhas de Estudo', studyTrails }) {
+export default function ListStudyTrails({ title = 'Trilhas de Estudo', studyTrails, hasAuthor=true }) {
 
     return (
         <div className="list-study-trails">
             <h1>{title}</h1>
             {studyTrails?.map((studyTrail) => (
-                <StudyTrailCard key={`${studyTrail.id}`} studyTrail={studyTrail} />
+                <StudyTrailCard key={`${studyTrail.id}`} studyTrail={studyTrail} hasAuthor={hasAuthor} />
             ))}
         </div>  
     );
--- src/components/StudyTrailCard/index.js
+++ src/components/StudyTrailCard/index.js
@@ -1,14 +1,16 @@
 import { useState } from 'react';
 import './styles.css';
 
-export default function StudyTrailCard({ studyTrail }) {
+export default function StudyTrailCard({ studyTrail, hasAuthor }) {
     const [collapsed, setCollapsed] = useState(true);
     return (
         <div className="card">
             <div className="content">
                 <div className="info-area">
                     <h3>{studyTrail.title}</h3>
-                    <p>por {studyTrail.user.name}</p>
+                    {hasAuthor && (
+                        <p>por {studyTrail.user.name}</p>
+                    )}
                 </div>
                 <div className="actions">
                     <span className="info" onClick={() => setCollapsed((state) => !state)}>{collapsed ? 'Mais informações' : 'Menos informações'}</span>
