210369f92cd06e304c6d3163e8a7e0f8bfe5b5e3
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-09-17 20:32:30 -0300

    fix(Header): update user signed info

--- src/components/template/Header/index.js
+++ src/components/template/Header/index.js
@@ -3,19 +3,31 @@ import './styles.css';
 
 import Logo from '../../assets/logo.svg';
 import { useLearnModal } from '../../../contexts/ModalContextApiHook';
+import { useLearnAuth } from '../../../contexts/AuthContextApiHook';
+import { FaUserCircle } from 'react-icons/fa';
+import { FiLogOut } from 'react-icons/fi';
 
 export default function Header() {
     const { showModal } = useLearnModal();
+    const { signed, user, signOut } = useLearnAuth();
 
     return (
         <header>
             <BaseContainer>
                 <div className="content">
                     <img src={Logo} alt="" className="logo" />
-                    <div className="actions">
-                        <button className="learn-btn learn-btn-primary" onClick={() => showModal('createUser')}>Cadastre-se</button>
-                        <button className="learn-btn learn-btn-primary-outline" onClick={() => showModal('loginUser')}>Entre</button>
-                    </div>
+                    {signed ? (
+                        <div className="user-info">
+                            <FaUserCircle className="icon" />
+                            <span>{user.name}</span>
+                            <FiLogOut className="logout-icon" onClick={() => signOut()} />
+                        </div>
+                    ) : (
+                        <div className="actions">
+                            <button className="learn-btn learn-btn-primary" onClick={() => showModal('createUser')}>Cadastre-se</button>
+                            <button className="learn-btn learn-btn-primary-outline" onClick={() => showModal('loginUser')}>Entre</button>
+                        </div>
+                    )}
                 </div>
             </BaseContainer>
         </header>
