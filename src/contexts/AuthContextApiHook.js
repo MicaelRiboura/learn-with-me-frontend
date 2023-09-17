dc14b178c5dd5053cd29c57e09688b29006430d2
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-09-17 15:59:57 -0300

    feat: create AuthContextApiHook

---
+++ src/contexts/AuthContextApiHook.js
@@ -0,0 +1,66 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const LearnAuthContext = createContext({});
+
+const LearnAuthProvider = ({ children }) => {
+    const [signed, setSigned] = useState(false);
+    const [user, setUser] = useState();
+
+    useEffect(() => {
+        const storagedSigned = localStorage.getItem('@Learn:signed');
+        const storagedUser = localStorage.getItem('@Learn:user');
+
+        if (storagedUser) {
+            setUser(JSON.parse(storagedUser));
+        }
+
+        if (storagedSigned === 'true') {
+            setSigned(true);
+        }
+
+    }, []);
+
+    const sign = async ({ email, password }) => {
+        try {
+            const url = 'http://127.0.0.1:5000/users/login';
+            const formData = new FormData();
+            formData.append('email', email);
+            formData.append('password', password);
+    
+            const responseJson = await fetch(url, {
+                method: 'post',
+                body: formData,
+            });
+    
+            const response = await responseJson.json();
+            setUser(response);
+            localStorage.setItem('@Learn:signed', 'true');
+            localStorage.setItem('@Learn:user', JSON.stringify(response));
+
+            return response;
+        } catch(error) {
+            console.error('Error:', error);
+            return {};
+        };
+    }
+
+    const signOut = () => {
+        localStorage.clear();
+        setUser(undefined);
+        setSigned(false);
+    }
+
+    return <LearnAuthContext.Provider value={{
+        signed,
+        user,
+        sign,
+        signOut,
+    }}>{children}</LearnAuthContext.Provider>;
+};
+
+const useLearnAuth = () => {
+    const context = useContext(LearnAuthContext);
+    return context;
+};
+
+export { LearnAuthProvider, useLearnAuth };
