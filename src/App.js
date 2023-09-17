d2fd01793f654f0ea9e568ffdfbd82d0e37638ca
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-09-17 15:19:46 -0300

    feat(App): add modals

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import './App.css';
 import Header from './components/template/Header';
 import Banner from './components/template/Banner';
 import Content from './components/template/Content';
+import Modal from './components/template/Modal';
+import CreateUserForm from './components/CreateUserForm';
+import LoginUserForm from './components/LoginUserForm';
 
 
 function App() {
   return (
     <div className="App">
+      {/* <Modal title="Increva-se">
+        <CreateUserForm />
+      </Modal> */}
+      {/* <Modal title="Entrar">
+        <LoginUserForm />
+      </Modal> */}
         <Header />
         <Banner />
         <Content />
