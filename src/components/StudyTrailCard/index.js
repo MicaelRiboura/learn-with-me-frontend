d5e1461c9bafcffec4933f8c8553c2927456060c
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-09-17 14:38:53 -0300

    feat: create StudyTrailCard component

---
+++ src/components/StudyTrailCard/index.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import './styles.css';
+
+export default function StudyTrailCard({ studyTrail }) {
+    const [collapsed, setCollapsed] = useState(true);
+    return (
+        <div className="card">
+            <div className="content">
+                <div className="info-area">
+                    <h3>{studyTrail.title}</h3>
+                    <p>por {studyTrail.user.name}</p>
+                </div>
+                <div className="actions">
+                    <span className="info" onClick={() => setCollapsed((state) => !state)}>{collapsed ? 'Mais informações' : 'Menos informações'}</span>
+                    <span className="start">Comece</span>
+                </div>
+            </div>
+            <div className={`description ${collapsed && 'collapsed'}`}>
+                {studyTrail.description}
+            </div>
+        </div>
+    );
+}
