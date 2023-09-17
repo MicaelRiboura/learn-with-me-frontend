15854d8daeade82592d8924e0661f9c3d94f1971
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-09-17 20:32:07 -0300

    feat(LoginUserForm): create submit and validations in form

--- src/components/LoginUserForm/index.js
+++ src/components/LoginUserForm/index.js
@@ -1,17 +1,62 @@
+import { toast } from "react-toastify";
+import { useValidationForms } from "../../hooks/useValidationForms";
+import { useLearnModal } from "../../contexts/ModalContextApiHook";
+import { useLearnAuth } from "../../contexts/AuthContextApiHook";
+
 export default function LoginUserForm() {
+    const { validateRequiredFields, validateEmailField } = useValidationForms();
+    const { hideModal } = useLearnModal();
+    const { sign } = useLearnAuth();
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        console.log('data: ', e.target.elements);
+
+        const email = e.target.elements['email'].value;
+        const password = e.target.elements['password'].value;
+
+        let validationErrors = validateRequiredFields([
+            { input: email, msg: 'O campo e-mail é obrigatório' },
+            { input: password, msg: 'O campo senha é obrigatório' },
+        ]);
+
+        validationErrors += validateEmailField(email);
+
+        if (validationErrors === 0) {
+            const formData = new FormData();
+            formData.append('email', email);
+            formData.append('password', password);
+
+            try {
+                const response = await sign({ email, password });
+                console.log('response: ', response);
+
+                if (!response.message) {
+                    toast.success(`Olá, ${response.name}!`);
+                    hideModal('loginUser');
+                } else {
+                    toast.error(response.message);
+                }
+            } catch (error) {
+                console.error('Error:', error);
+                toast.error('Erro no cadastro!');
+            }
+        }
+    }
+
     return (
-        <div>
+        <form method="post" onSubmit={onSubmit}>
             <div className="input-group">
                 <label htmlFor="">E-mail</label>
-                <input type="text" className="form-input" />
+                <input name="email" type="text" className="form-input" />
             </div>
             <div className="input-group">
                 <label htmlFor="">Senha</label>
-                <input type="password" className="form-input" />
+                <input name="password" type="password" className="form-input" />
             </div>
             <div className="form-button-area">
-                <button className="learn-btn learn-btn-primary">Entrar</button>
+                <button type="submit" className="learn-btn learn-btn-primary">Entrar</button>
             </div>
-        </div>
+        </form>
     )
 }
