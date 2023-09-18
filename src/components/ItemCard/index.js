6e2c388a3169bcee5944004524f6529138bd5d76
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-09-17 22:47:50 -0300

    feat(ItemCard): create delete method

--- src/components/ItemCard/index.js
+++ src/components/ItemCard/index.js
@@ -2,37 +2,63 @@ import { useState } from 'react';
 import { AiFillPlayCircle, AiFillFileText } from 'react-icons/ai';
 import { FaTrashAlt } from 'react-icons/fa';
 import './styles.css';
+import { toast } from 'react-toastify';
 
 export default function ItemCard({ item, hasDelete=false }) {
     const [collapsed, setCollapsed] = useState(true);
+
+    const [hasDeleted, setHasDeleted] = useState(false);
 
     const itemTypesDict = {
         'text': 'Texto',
         'video': 'Video',
     };
 
+    const deleteItem = (id) => {
+        const url = `http://localhost:5000/items/delete?id=${id}`;
+        fetch(url, {
+            method: 'delete',
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.message.includes('sucesso')) {
+                    toast.success('Item de estudo removido com sucesso!');
+                    setHasDeleted(true);
+                } else {
+                    toast.error(data.message);
+                }
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+
     return (
-        <div className="item-card">
-            <div className="content">
-                <div className="info-area">
-                    {item.type === 'video' && <AiFillPlayCircle className="item-icon" />}
-                    {item.type === 'text' && <AiFillFileText className="item-icon" />}
-                    <div className="info">
-                        <h3>{item.title}</h3>
-                        <p>{itemTypesDict[item.type]}</p>
+        <>
+            {!hasDeleted && (
+                <div className="item-card">
+                    <div className="content">
+                        <div className="info-area">
+                            {item.type === 'video' && <AiFillPlayCircle className="item-icon" />}
+                            {item.type === 'text' && <AiFillFileText className="item-icon" />}
+                            <div className="info">
+                                <h3>{item.title}</h3>
+                                <p>{itemTypesDict[item.type]}</p>
+                            </div>
+                        </div>
+                        <div className="actions">
+                            <span className="info" onClick={() => setCollapsed((state) => !state)}>{collapsed ? 'Mais informações' : 'Menos informações'}</span>
+                            <a href={item.resource} target="_blank" rel="noreferrer"><span className="start">Acesse</span></a>
+                            {hasDelete && (
+                                <FaTrashAlt className="delete-icon" onClick={() => deleteItem(item.id)} />
+                            )}
+                        </div>
+                    </div>
+                    <div className={`description ${collapsed && 'collapsed'}`}>
+                        {item.description}
                     </div>
                 </div>
-                <div className="actions">
-                    <span className="info" onClick={() => setCollapsed((state) => !state)}>{collapsed ? 'Mais informações' : 'Menos informações'}</span>
-                    <a href={item.resource} target="_blank" rel="noreferrer"><span className="start">Acesse</span></a>
-                    {hasDelete && (
-                        <FaTrashAlt className="delete-icon" />
-                    )}
-                </div>
-            </div>
-            <div className={`description ${collapsed && 'collapsed'}`}>
-                {item.description}
-            </div>
-        </div>
+            )}
+        </>
     );
 }
