f9f52fa29f2b48a6e7027f021237cb5b7ddc9ded
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-09-17 22:02:07 -0300

    feat(StudyTrailCard): add link to StudyTrailDetails

--- src/components/StudyTrailCard/index.js
+++ src/components/StudyTrailCard/index.js
@@ -1,10 +1,14 @@
 import { useState } from 'react';
 import './styles.css';
+import { useLearnPages } from '../../contexts/PagesContextApiHook';
 
 export default function StudyTrailCard({ studyTrail, hasAuthor }) {
+    const { changePage } = useLearnPages();
+
     const [collapsed, setCollapsed] = useState(true);
+
     return (
-        <div className="card">
+        <div className="study-trail-card">
             <div className="content">
                 <div className="info-area">
                     <h3>{studyTrail.title}</h3>
@@ -14,10 +18,11 @@ export default function StudyTrailCard({ studyTrail, hasAuthor }) {
                 </div>
                 <div className="actions">
                     <span className="info" onClick={() => setCollapsed((state) => !state)}>{collapsed ? 'Mais informações' : 'Menos informações'}</span>
-                    <span className="start">Comece</span>
+                    <span className="start" onClick={() => changePage('StudyTrailDetails', { studyTrailId: studyTrail.id })}>Comece</span>
                 </div>
             </div>
-            <div className={`description ${collapsed && 'collapsed'}`}>
+            <div 
+                className={`description ${collapsed && 'collapsed'}`}>
                 {studyTrail.description}
             </div>
         </div>
